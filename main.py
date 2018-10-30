686de6560b2ad6da0bcd76b8cc09b25a893329c0
Sashok <alex15023@yandex.ru>
2018-10-30 17:26:29 +0000

    Answering

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ def main():
         api_token = f.read().strip()
 
     vk_session = vk_api.VkApi(token=api_token)
-
+    api = vk_session.get_api()
     longpoll = VkBotLongPoll(vk_session, '173227835')
     print('Слушаем')
     for event in longpoll.listen():
 
         if event.type == VkBotEventType.MESSAGE_NEW:
             print(event.obj.text)
-            send_msg(event.from_id, event.obj.text, vk_session)
+            send_msg(event.obj.from_id, event.obj.text, api)
 
 
 if __name__ == '__main__':
